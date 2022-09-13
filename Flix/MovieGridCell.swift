928f99f9435293a42ddbd79695c834a214e56dde
gbell331 <111946282+gbell331@users.noreply.github.com>
2022-09-13 14:50:10 -0400

    updated for the full flix app files to repository

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by student on 9/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
